85ed9e52c854814eafa0fdb577cf33535e76949c
Hunter <hmvitous@yahoo.com>
2020-06-08 16:03:16 +0200

    Creates an if statement that adds one to a users entries when that submit an image

--- server.js
+++ server.js
@@ -61,9 +61,24 @@ app.get("/profile/:id", (req, res) => {
       found = true;
       return res.json(user);
     }
-  })
+  });
   if (!found) {
-    res.status(400).json('not found')
+    res.status(400).json("not found");
+  }
+});
+
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json("not found");
   }
 });
 
