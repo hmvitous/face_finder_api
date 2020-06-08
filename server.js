151be573fe7e8cf46ee0871f28317f52a76a8bc3
Hunter <hmvitous@yahoo.com>
2020-06-08 15:11:08 +0200

    Creates and simple signin with hardcoded values

--- server.js
+++ server.js
@@ -1,12 +1,45 @@
-const express = require('express')
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('working')
-})
+app.use(bodyParser.json());
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "Joe",
+      email: "momm@mom.com",
+      password: "mommy",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "124",
+      name: "Jib",
+      email: "dad@mom.com",
+      password: "daddy",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
 
-app.listen(3000, ()=> {
-  console.log('running')
-})
+app.get("/", (req, res) => {
+  res.send("working");
+});
 
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("running");
+});
