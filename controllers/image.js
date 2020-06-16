c8aa6807a51f761226409e58db81a11ee56191a8
Hunter <hmvitous@yahoo.com>
2020-06-16 15:04:15 +0200

    Adds the api call to increase security

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,17 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "daac70cd5aa14b5fa261d512d1db8212",
+});
+
+const handleApiCall = (req, res) => {
+  app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+  .then(data => {
+    res.json(data)
+  })
+  .catch(err => res.status(400).json('unable to work with API'))
+};
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db("users")
@@ -12,4 +26,5 @@ const handleImage = (req, res, db) => {
 
 module.exports = {
   handleImage: handleImage,
+  handleApiCall: handleApiCall
 };
