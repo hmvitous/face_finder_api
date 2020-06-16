16dfd7d6f5c5cdda1b30dcca9b8fd020b9b69fcf
Hunter <hmvitous@yahoo.com>
2020-06-16 15:45:22 +0200

    Changes port option to work with heroku

--- server.js
+++ server.js
@@ -47,6 +47,6 @@ app.post("/imageurl", (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log("running");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`running on ${process.env.PORT}`);
 });
